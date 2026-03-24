185423f72046535ba4c4e082532f76623bfcb1d1
StuartGJohnson <stuart.g.johnson@gmail.com>
2026-03-23 19:25:43 -0700

    Revert "fix double lidar launch."
    This reverts commit 9e172f074297cb2db3508df7a7b0e4686fbda632.

--- src/ugv_main/ugv/launch/bringup_rtabmap.launch.py
+++ src/ugv_main/ugv/launch/bringup_rtabmap.launch.py
@@ -73,6 +73,14 @@ def generate_launch_description():
         )
     )
 
+    # lidar launch
+    # TODO: move the launch file to a ros2 package, so the launch works from anywhere
+    laser_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            'ld19.launch.py'
+        )
+    )
+
     # realsense launch
     realsense_launch = GroupAction(
         actions=[
@@ -222,6 +230,7 @@ def generate_launch_description():
         #fuser_node,
         robot_state_launch,
         realsense_launch,
+        laser_bringup_launch,
         rf2o_node,
         nav2_launch,
         pointcloud_node,
