bb5af5c1ddf4033efcea294330cc047c52266dcf
StuartGJohnson <stuart.g.johnson@gmail.com>
2026-03-28 04:17:47 -0700

    Update to /odom in rf2o launch.

--- src/ugv_main/ugv/launch/bringup_rtabmap_rf2o.launch.py
+++ src/ugv_main/ugv/launch/bringup_rtabmap_rf2o.launch.py
@@ -151,7 +151,7 @@ def generate_launch_description():
         output='screen',
         parameters=[{
             'laser_scan_topic' : '/scan',
-            'odom_topic' : '/odom_rf2o',
+            'odom_topic' : '/odom',
             'publish_tf' : True,
             'base_frame_id' : 'base_footprint',
             'odom_frame_id' : 'odom',
